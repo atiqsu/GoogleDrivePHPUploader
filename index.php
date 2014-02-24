0600e1fde9f1e57b4e3a771d42a1c6b03f3448f1
Prathik Raj <prathikraj@ymail.com>
2014-02-24 17:01:40 +0530

    Store client id and client secret in client_secret.json

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 require_once 'google-api-php-client/src/Google_Client.php';
 require_once 'google-api-php-client/src/contrib/Google_DriveService.php';
 
+$client_secret = json_decode(file_get_contents("client_secret.json"));
+
 $client = new Google_Client();
 // Get your credentials from the console
-$client->setClientId('470041835120-3s3ldb989c8i1ude06nhs949al6u9kj7.apps.googleusercontent.com');
-$client->setClientSecret('mUvcsJoXI1fdm8jcdGXqhRDd');
-$client->setRedirectUri('urn:ietf:wg:oauth:2.0:oob');
+$client->setClientId($client_secret->installed->client_id);
+$client->setClientSecret($client_secret->installed->client_secret);
+$client->setRedirectUri($client_secret->installed->redirect_uris[0]);
 $client->setScopes(array('https://www.googleapis.com/auth/drive'));
 
 $service = new Google_DriveService($client);
